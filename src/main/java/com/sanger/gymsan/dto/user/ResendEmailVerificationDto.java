1d7451fa54409c20f60cc106198475a20f5201a8
Sebastian Sangermano <seba_sanger@hotmail.com>
2025-12-05 03:39:48 -0300

    fixed resend mail

--- src/main/java/com/sanger/gymsan/dto/user/ResendEmailVerificationDto.java
+++ src/main/java/com/sanger/gymsan/dto/user/ResendEmailVerificationDto.java
@@ -1,6 +1,5 @@
 package com.sanger.gymsan.dto.user;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,8 +16,5 @@ public class ResendEmailVerificationDto {
 
     @NotNull
     private Long id;
-
-    @NotBlank
-    private String urlRedirect;
 
 }
